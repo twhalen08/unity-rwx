50506caeaf0ca235b09bfd4df1de3a8ee9b30fef
twhalen08 <twhalen08@gmail.com>
2025-12-26 18:22:56 -0500

    Tag prototype instance roots with current material tag

--- rwx/RWXPrototypeParser.cs
+++ rwx/RWXPrototypeParser.cs
@@ -105,6 +105,11 @@ namespace RWXLoader
             // Create a new GameObject for this prototype instance
             var instanceObject = new GameObject($"Proto_{prototypeName}");
             instanceObject.transform.SetParent(context.currentObject.transform);
+            
+            // Propagate the current material tag so actions can target the entire prototype
+            var protoTag = instanceObject.AddComponent<RWXTag>();
+            protoTag.TagId = context.currentMaterial?.tag ?? 0;
+            protoTag.TextureName = context.currentMaterial?.texture;
 
             // Do **not** bake the current transform directly onto the instance here. The clump that owns this
             // instance will receive the accumulated transform at ClumpEnd, and applying it twice caused
